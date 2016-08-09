d7b3b608e1818de70da4fcde1c22a4350cd112d4
krzaq <git@krzaq.cc>
2016-08-09 17:13:58 +0200

    removed unnecessary includes and empty lines

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,5 @@
 #include "includes.hpp"
 
-#include "../resource/resource.hpp"
-#include "../resource/storage/default_storage.hpp"
-#include "../resource/cleanup/function_deleter.hpp"
-#include "../resource/copy/not_customized.hpp"
-#include "../resource/copy/disable_copy_enable_move.hpp"
-#include "../resource/copy/disable_copy_and_move.hpp"
-
 void test_fd();
 void test_malloc();
 void test_new();
@@ -19,12 +12,4 @@ auto main() -> int
 	test_malloc();
 	test_mutex_locker();
 }
-
-//#if 0
-
-
-
-//#endif
-
-
 
